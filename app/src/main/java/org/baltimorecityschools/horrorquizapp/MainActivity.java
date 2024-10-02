d9f791dc8d759de1be5890d559da1eed8a2838cd
Gray <1157667@bcpss.org>
2024-10-02 14:51:11 -0400

    i reworked my code and added my choice's for my correct answers in the code

--- app/src/main/java/org/baltimorecityschools/horrorquizapp/MainActivity.java
+++ app/src/main/java/org/baltimorecityschools/horrorquizapp/MainActivity.java
@@ -1,10 +1,12 @@
 package org.baltimorecityschools.horrorquizapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,45 +18,72 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     RadioButton WrongBTN1,WrongBTN2;
-    Button DoneBTN;
+    TextView QuestiovTv;
+    Button NextBTN;
     String toastMsg;
     Toast myToast;
     int duration,score;
     Intent GoToSA;
+    String userChoice;
+    String correctAnss;
+    Questions q1,q2,q3,q4,q5,q6,q7,q8,q9,q10, currentq;
+    Questions[] questionss;
+    int currentIndex;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         WrongBTN1 = (RadioButton) findViewById(R.id.WrongBTN1);
+        currentIndex = 0;
+        currentq = q1;
+        q1 = new Questions("You're lost in a graveyard at midnight all by yourself. And you see a tall figure in the shodaw;s looking at you with white eyes. What do you do?",);
+        q2 = new Questions("If you hear tapping on your window at night and turn to see a figure looking at you what will you do?",);
+        q3 = new Questions("There’s a man following you and as you start to walk fast and he starts running at you, what's your next move?",);
+        q4 = new Questions("You’re trapped in a cabin with two famous horror movie characters: Jason Voorhees from Friday the 13th and Freddy Krueger from A Nightmare on Elm Street. You have to choose one to face, knowing you may survive",);
+        q5 = new Questions("You’re wandering through the Texas countryside when you encounter Leatherface from The Texas Chainsaw Massacre. You spot two ways to escape?",);
+        q6 = new Questions("You wake up chained in a dark room, and Jigsaw** fromSaw* gives you four choices to escape?",);
+        q7 = new Questions("You’re stuck in a creepy hotel, and Jack Torrance from The Shining is coming after you with an axe. You see two possible exits?",);
+        q8 = new Questions("You find yourself in a rural area where The Creeper from Jeepers Creepers is hunting for his next victim. You have two ways to escape?",);
+        q9 = new Questions("You’re in a haunted house, and The Babadook is lurking in the shadows. You have two options to survive?",);
+        q10 = new Questions("You’re being stalked by The Predator in a dense jungle. You have two options for survival?",);
+        questionss = new Questions[] {q1,q2,q3,q4,q5,q6,q7,q8,q9,q10};
         WrongBTN2 = (RadioButton) findViewById(R.id.WrongBTN2);
-        DoneBTN = (Button) findViewById(R.id.DoneBTN);
+        NextBTN = (Button) findViewById(R.id.NextBTN);
+        QuestiovTv = (TextView) findViewById(R.id.QuestiovTv);
         toastMsg = "You Got it Correct";
 
 
-        DoneBTN.setOnClickListener(new View.OnClickListener() {
+        NextBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if (WrongBTN1.isChecked()){
-                    score+=9;
-                    toastMsg = "You Got it Correct";
-                }
-                else if (WrongBTN2.isChecked()){
-                    toastMsg = "Loser";
-                }
 
                 duration = Toast.LENGTH_SHORT;
                 myToast = Toast.makeText(MainActivity.this, toastMsg, duration);
                 myToast.show();
 
-                GoToSA = new Intent(MainActivity.this, ScoreActivity.class);
-                GoToSA.putExtra("score",score);
-                startActivity(GoToSA);
+                if (currentIndex == 9) {
+                    GoToSA = new Intent(MainActivity.this, ScoreActivity.class);
+                    GoToSA.putExtra("score",score);
+                    startActivity(GoToSA);
+
+                }
+                else {
+                    currentIndex++;
+                    currentq = questionss[currentIndex];
+                    QuestiovTv.setText(currentq.getqText());
+
+                }
+
 
             }
         });
+        
+
+
 
         }
     }
--- app/src/main/java/org/baltimorecityschools/horrorquizapp/Questions.java
+++ app/src/main/java/org/baltimorecityschools/horrorquizapp/Questions.java
@@ -2,24 +2,71 @@ package org.baltimorecityschools.horrorquizapp;
 
 public class Questions {
 
+    private String choiceA;
+    private String choiceB;
+    private String choiceC;
+    private String choiceD;
     private String qText;
-    private boolean correctAns;
+    private String correctAns;
 
     public Questions() {
         qText = " ";
-        correctAns = false;
+        correctAns = " ";
+        choiceA = " ";
+        choiceB = " ";
+        choiceC = " ";
+        choiceD = " ";
     }
 
-    public Questions(String sqText, boolean scorrectAns) {
+    public Questions(String sqText, String scorrectAns, String schoiceA, String schoiceB,String schoiceC,String schoiceD) {
         correctAns = scorrectAns;
         qText = sqText;
+        choiceA = schoiceA;
+        choiceB = schoiceB;
+        choiceC = schoiceC;
+        choiceD = schoiceD;
     }
 
     public  String getqText() {return qText;}
-    public boolean getcorrectAns() {return correctAns;}
+
+    public String getChoiceA() {
+        return choiceA;
+    }
+
+    public void setChoiceA(String choiceA) {
+        this.choiceA = choiceA;
+    }
+
+    public String getChoiceB() {
+        return choiceB;
+    }
+
+    public void setChoiceB(String choiceB) {
+        this.choiceB = choiceB;
+    }
+
+    public String getChoiceC() {
+        return choiceC;
+    }
+
+    public void setChoiceC(String choiceC) {
+        this.choiceC = choiceC;
+    }
+
+    public String getChoiceD() {
+        return choiceD;
+    }
+
+    public void setChoiceD(String choiceD) {
+        this.choiceD = choiceD;
+    }
+
+    public String getCorrectAns() {
+        return correctAns;
+    }
 
     public  void setqText(String newQtext) {qText = newQtext; }
-    public void setCorrectAns(Boolean newcorrectAns) {correctAns = newcorrectAns;}
+    public void setCorrectAns(String newcorrectAns) {correctAns = newcorrectAns;}
 
     @Override
     public String toString() {
